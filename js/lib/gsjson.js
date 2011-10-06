108e01b2e88d14f2c43226a48751b02e912e012b
Riley W <riley.wills@acu.edu>
2011-10-06 14:30:14 -0500

    Starting the framework for a GSJSON jQuery plug-in

---
+++ js/lib/gsjson.js
@@ -0,0 +1,13 @@
+/**
+ * jQuery GSJSON - jQuery plug-in
+ * Version 0.0.1, Unusable
+ *
+ * Usage:
+ *  $(element).GSJSON({
+ *    url:'http://localhost/search',
+ *    frontend:'json_frontend',
+ *    site:'default_collection',
+ *    callback: null
+ *  });
+ **/
+$(function() { })(jQuery);
