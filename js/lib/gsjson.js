4befacf3731d38219bdc6ea791d2b35b17a3ce7a
Riley W <riley.wills@acu.edu>
2011-10-06 14:56:05 -0500

    Added test-gsjson.html for jQuery GSJSON

--- js/lib/gsjson.js
+++ js/lib/gsjson.js
@@ -1,13 +1,27 @@
 /**
  * jQuery GSJSON - jQuery plug-in
- * Version 0.0.1, Unusable
- *
- * Usage:
- *  $(element).GSJSON({
- *    url:'http://localhost/search',
- *    frontend:'json_frontend',
- *    site:'default_collection',
- *    callback: null
- *  });
+ * https://github.com/steveathon/GSJSON
  **/
-$(function() { })(jQuery);
+$(function($) { 
+	$.GSJSON = function(e,o){
+		var d = {
+			search_appliance: null,
+			site: 'default_frontend',
+			client: 'default_collection',
+			oneboxCallback: function () {},
+			resultCallback: function () {}
+		};
+		
+		// Reference current instance of the object
+		var gsjson = this;
+		
+		// Merge d and user-provided options
+		gsjson.settings = {};
+		var start = function () {
+			gsjson.settings = $.extend({},d,o);
+			gsjson.e = e;
+		}
+		
+		start();
+	}
+})(jQuery);
