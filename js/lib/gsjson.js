72a134b35a2cd1ed9859899ba2fdd7a50501214d
Riley W <riley.wills@acu.edu>
2011-10-06 16:30:30 -0500

    Very rough example

--- js/lib/gsjson.js
+++ js/lib/gsjson.js
@@ -2,26 +2,23 @@
  * jQuery GSJSON - jQuery plug-in
  * https://github.com/steveathon/GSJSON
  **/
-$(function($) { 
-	$.GSJSON = function(e,o){
-		var d = {
-			search_appliance: null,
-			site: 'default_frontend',
-			client: 'default_collection',
-			oneboxCallback: function () {},
-			resultCallback: function () {}
+;(function($) { 
+	$.fn.GSJSON = function (o) {
+		var s = {
+			uri:null,
+			collection:'default_collection',
+			frontend:'default_frontend',
+			query:null,
+			callback: function (e) { return $('<div>'+e.T+'</div>'); }
 		};
 		
-		// Reference current instance of the object
 		var gsjson = this;
-		
-		// Merge d and user-provided options
-		gsjson.settings = {};
-		var start = function () {
-			gsjson.settings = $.extend({},d,o);
-			gsjson.e = e;
-		}
-		
-		start();
+		gsjson.settings = $.extend({},s,o);
+		$.getJSON(gsjson.settings.uri+'/search?q='+gsjson.settings.query+'&client='+gsjson.settings.frontend+'&site='+gsjson.settings.collection+'&output=xml_no_dtd&proxystylesheet=json_frontend&proxyreload=1&callback=?',function(data){
+			$.each(data.GSP.RES.R,function (key,val) {
+				if(gsjson.settings.callback)
+					$(gsjson.settings.callback(val)).appendTo($(gsjson));
+			})
+		});
 	}
 })(jQuery);
